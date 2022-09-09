462867cbf7f2a93fa2b76cfb18d95fe108e9ea8e
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2022-09-09 14:30:23 -0500

    Fixeando todos los modos de juegos ademas de terminar de hacer el modo de juego custom

--- Casino/casino.cpp
+++ Casino/casino.cpp
@@ -1,37 +1,41 @@
-// Tengo que refactorizar todo el codigo, haciendo uso de los include
-// Asi no queda tan largo este archivo :>
-
 #include <bits/stdc++.h>
-#include "funciones.h"
+#include <string.h>
+#include "modoJuegosCasino.h"
 using namespace std;
 
+// Cambios en Stage
 int main() {
+    system("cls");
+    cout << "\n\tWelcome to the Casino" << endl;
+
     string playerName;
     float balance;
-    cout << "\n\tWelcome to the Casino" << endl;
-    cout << "Enter your name to play: ";
+
+    cout << "But, before that ..." << endl; 
+    cout << "Enter your name: ";
     getline(cin, playerName);
 
-    do {
-        cout << "Enter your inicial balance to play: ";
-        cin >> balance;
-    } while (balance < 1);
 
-    displayGameModes();
-    int gameMode, multiplier;
-    cout << "So ... wich one you wanna play?" << endl;
-    cout << "[Default gamemode is \"1\"]" << endl;
-    cout << "-> ";
-    cin >> gameMode;
-    preGameHandler(gameMode, multiplier);
+    switch(displayGames()) {
+        case 1: {
+            cout << "(You're playing the \"Guess a Number\" game mode)" << endl;
+            cout << "(So, we recommend that the inicial balance should be bigger than 10)" << endl; 
+            do {
+                cout << "Enter your inicial balance to play [$]: ";
+                cin >> balance;
+            } while (balance < 1);
+
+            displayGameModes();
+            int gameMode, multiplier;
+            cout << "So ... wich one you wanna play?" << endl;
+            cout << "[Default gamemode is \"1\"]" << endl;
+            cout << "-> ";
+            cin >> gameMode;
+            preGameHandler(gameMode, multiplier, playerName, balance);
+            break;
+        }
+        default:
+            cout << "There isn't any game mode which fits with the option entered ..." << endl;
+    }
     return 0;
 }
-
-
-
-
-
-
-
-
-
