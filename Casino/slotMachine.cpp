116e07bc255e1f6a549b87250bffa786994b677b
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2022-09-09 14:45:55 -0500

    Nada que comentar

--- Casino/slotMachine.cpp
+++ Casino/slotMachine.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+int generateRandomNumber() {
+    srand(time(NULL));
+    return (rand() % (10 - 1));
+}
+
+class machineSlots {
+    public:
+        int number;
+};
+
 int main() {
-    
+    int FPS = 30;
+
+    for (int i = 1; i <= FPS; i++) {
+        int slot1, slot2, slot3;
+
+        [slot1, slot2, slot3] = generateRandomNumber();
+
+        cout << slot1 << " " << slot2 << " " << slot3 << endl;
+    }
+
+
+    return 0;
 }
