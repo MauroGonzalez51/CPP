6cd08ced5f0a36bcd9482a0b19a7705b834fc87e
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2022-09-22 20:41:14 -0500

    Cosas

--- Code/WeasRandom/trabajadores.cpp
+++ Code/WeasRandom/trabajadores.cpp
@@ -1,106 +1,96 @@
-#include <bits/stdc++.h> 
+/**
+ *  @autor mauroGonzalez
+ *  @code T00067622
+ * 
+ * */
+
+#include <bits/stdc++.h>
 
 using namespace std;
 
+class Trabajadores {
+    public:
+        string codigo;
+        int grupo, salarioSemanal;
+        
+        bool validarGrupo() { 
+            return (((this -> grupo != 1) && (this -> grupo != 2)) ? true : false);
+        }
+
+        bool validarSalario() {
+            return ((this -> salarioSemanal < 1) ? true : false);
+        }
+};
+
 int main() {
-    int cantidadTrabajadores, nuevoGrupo = 0;
+    int cantidadTrabajadores, cambiosGrupo = 0;
+
+    array <float, 3> promedioGrupoUno;
+    array <float, 3> promedioGrupoDos;
+
+    promedioGrupoUno.fill(0.0);
+    promedioGrupoDos.fill(0.0);
 
     do {
-        cout << "Ingrese la cantidad de trabajadores: ";
+        cout << "Cantidad de Trabajadores: ";
         cin >> cantidadTrabajadores;
     } while (cantidadTrabajadores < 1);
 
+    Trabajadores trabajadores[cantidadTrabajadores];
     system("cls");
-
-    string codigoTrabajadores[cantidadTrabajadores];
-    int grupo[cantidadTrabajadores];
-    int salarioSemanal[cantidadTrabajadores];
-
-    // Pidiendo los datos :>
     for (int i = 0; i < cantidadTrabajadores; i++) {
         cout << endl;
-        cout << "[EMPLEADO " << i + 1 << "] Ingrese su codigo/identificacion: ";
-        cin >> codigoTrabajadores[i];
+        cout << "[EMPLEADO " << i + 1 << "] Ingrese su codigo: ";
+        cin >> trabajadores[i].codigo;
 
         do {
             cout << "[EMPLEADO " << i + 1 << "] Ingrese el grupo al que pertenece: ";
-            cin >> grupo[i];
-        } while ((grupo[i] != 1) && (grupo[i] != 2));
+            cin >> trabajadores[i].grupo;
+        } while (trabajadores[i].validarGrupo());
 
         do {
             cout << "[EMPLEADO " << i + 1 << "] Ingrese su salario semanal: ";
-            cin >> salarioSemanal[i];
-        } while (salarioSemanal[i] < 1);
-    }
+            cin >> trabajadores[i].salarioSemanal;
+        } while (trabajadores[i].validarSalario());
 
-    // Aplicando el aumento :>
-
-    for (int i = 0; i < cantidadTrabajadores; i++) {
-        (grupo[i] == 1) ? salarioSemanal[i] += 250000 : salarioSemanal[i] +=  150000;
-    }
-
-    // Verificar el promedio de salario de cada grupo :>
-
-    float promedioGrupo1 = 0.0, promedioGrupo2 = 0.0;
-    int cantidadGrupo1 = 0, cantidadGrupo2 = 0;
-
-    for (int i = 0; i < cantidadTrabajadores; i++) {
-        if (grupo[i] == 1) {
-            promedioGrupo1 += salarioSemanal[i];
-            cantidadGrupo1++;
+        if (trabajadores[i].grupo == 1) {
+            trabajadores[i].salarioSemanal += 250000;
+            promedioGrupoUno[0] += trabajadores[i].salarioSemanal;
+            promedioGrupoUno[1]++;
         } else {
-            promedioGrupo2 += salarioSemanal[i];
-            cantidadGrupo2++;
+            trabajadores[i].salarioSemanal += 150000;
+            promedioGrupoDos[0] += trabajadores[i].salarioSemanal;
+            promedioGrupoDos[1]++;
         }
+
+        for (int i = 0; i < 60; i++) 
+            cout << "-";
+        
     }
 
-    if (cantidadGrupo1 != 0)
-        promedioGrupo1 /= cantidadGrupo1;
-
-    if (cantidadGrupo2 != 0)
-        promedioGrupo2 /= cantidadGrupo2;
-
-    // cout << "PRE Aumento" << endl;
-    // for (int i = 0; i < cantidadTrabajadores; i++) {
-    //     cout << codigoTrabajadores[i] << " " << grupo[i] << " " << salarioSemanal[i] << " " << endl;
-    // }
-
-
-    // cout << "POST Aumento" << endl;
-    // for (int i = 0; i < cantidadTrabajadores; i++) {
-    //     cout << codigoTrabajadores[i] << " " << grupo[i] << " " << salarioSemanal[i] << " " << endl;
-    // }
-
-    // Verificando si algun empleado del grupo 1 tiene salario mayor al promedio de su grupo :> 
+    if (promedioGrupoUno[1] != 0.0)
+        promedioGrupoUno[2] = promedioGrupoUno[0] / promedioGrupoUno[1];
+    
+    if (promedioGrupoDos[1] != 0.0) 
+        promedioGrupoDos[2] = promedioGrupoDos[0] / promedioGrupoDos[1];
 
     for (int i = 0; i < cantidadTrabajadores; i++) {
-        if ((salarioSemanal[i] > promedioGrupo1) && (grupo[i] == 1)) {
-            grupo[i] = 2;
-            nuevoGrupo++;
+        if ((trabajadores[i].grupo == 1) && (trabajadores[i].salarioSemanal > promedioGrupoUno[2])) {
+            trabajadores[i].grupo = 2;
+            cambiosGrupo++;
         }
     }
-
-    // Imprimiendo los datos
-    system("cls");
-    cout << "Nuevo Sueldo" << endl;
-    cout << "Cambiaron de Grupo: " << nuevoGrupo << " empleado(s)" << endl;
-
+    cout << endl;
+    for (int i = 0; i < 60; i++)
+        cout << "*";
+    
+    cout << endl;
+    cout << "Salarios [...]" << endl;
+    cout << "Cambios de Grupo: " << cambiosGrupo << endl;
 
     for (int i = 0; i < cantidadTrabajadores; i++) {
-        cout << "| Codigo: " << codigoTrabajadores[i] << " | Grupo: " << grupo[i] << " | Salario: " << salarioSemanal[i] << " |" << endl;
+        cout << "Codigo: " << trabajadores[i].codigo << " | Grupo: " << trabajadores[i].grupo << " | Salario: " << trabajadores[i].salarioSemanal << endl;
     }
-
+    
     return 0;
 }
-
-/*
-    PD: En el programa primero si aplica el aumento, luego se verifica si este "Nuevo"
-    salario es mayor que el promedio de su grupo.
-
-    En caso de que asi sea, se ca cambia al grupo 2
-
-    Lo hice de esta manera ya que al probar hacerlo al reves, todos los empleados del grupo 1,
-    pasaban directamente al grupo 2
-
-    Asi que asumí que de esta manera habria algunos que se quedan en el mismo grupo, otros que no
-*/
