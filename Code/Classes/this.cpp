5aba348c295c04e628074d945b76146b3acb2323
MauroGonzalez <mauroalonso.g.f2004@gmail.com>
2022-10-06 23:05:53 -0500

    this

---
+++ Code/Classes/this.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+class Numeros {
+    public:
+        std::vector <int> array;
+
+};
+
+int main() {
+    Numeros* numero = new Numeros();
+
+    return EXIT_SUCCESS;
+}
